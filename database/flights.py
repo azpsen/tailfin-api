27a1fc7eda3dbd6d3caf3ce9417363c119f8431d
april <aprl.petersen@gmail.com>
2024-01-08 15:12:25 -0600

    Remove redundant "total" from totals

--- database/flights.py
+++ database/flights.py
@@ -47,16 +47,16 @@ async def retrieve_totals(user: str, start_date: datetime = None, end_date: date
         {"$match": match},
         {"$group": {
             "_id": None,
-            "total_time": {"$sum": "$time_total"},
-            "total_solo": {"$sum": "$time_solo"},
-            "total_night": {"$sum": "$time_night"},
-            "total_pic": {"$sum": "$time_pic"},
-            "total_sic": {"$sum": "$time_sic"},
-            "total_instrument": {"$sum": "$time_instrument"},
-            "total_sim": {"$sum": "$time_sim"},
+            "time_total": {"$sum": "$time_total"},
+            "time_solo": {"$sum": "$time_solo"},
+            "time_night": {"$sum": "$time_night"},
+            "time_pic": {"$sum": "$time_pic"},
+            "time_sic": {"$sum": "$time_sic"},
+            "time_instrument": {"$sum": "$time_instrument"},
+            "time_sim": {"$sum": "$time_sim"},
             "time_xc": {"$sum": "$time_xc"},
-            "landings_day": {"$sum": "$takoffs_day"},
-            "landings_night": {"$sum": "$takeoffs_nights"},
+            "landings_day": {"$sum": "$landings_day"},
+            "landings_night": {"$sum": "$landings_night"},
 
         }
         },
@@ -71,12 +71,12 @@ async def retrieve_totals(user: str, start_date: datetime = None, end_date: date
     totals = result[0]
     async for log in flight_collection.find({"user": ObjectId(user)}):
         flight = FlightDisplaySchema(**flight_display_helper(log))
-        totals["total_xc_instr"] = totals.get("total_xc_instr", 0) + min(flight.time_xc, flight.dual_recvd)
-        totals["total_xc_solo"] = totals.get("total_xc_solo", 0) + min(flight.time_xc, flight.time_solo)
-        totals["total_xc_pic"] = totals.get("total_xc_pic", 0) + min(flight.time_xc, flight.time_pic)
-        totals["total_night_dual_recvd"] = totals.get("total_night_dual_recvd", 0) + min(flight.time_night,
-                                                                                         flight.dual_recvd)
-        totals["total_night_pic"] = totals.get("total_night_pic", 0) + min(flight.time_night, flight.time_pic)
+        totals["xc_dual_recvd"] = totals.get("xc_dual_recvd", 0) + min(flight.time_xc, flight.dual_recvd)
+        totals["xc_solo"] = totals.get("xc_solo", 0) + min(flight.time_xc, flight.time_solo)
+        totals["xc_pic"] = totals.get("xc_pic", 0) + min(flight.time_xc, flight.time_pic)
+        totals["night_dual_recvd"] = totals.get("night_dual_recvd", 0) + min(flight.time_night,
+                                                                             flight.dual_recvd)
+        totals["night_pic"] = totals.get("night_pic", 0) + min(flight.time_night, flight.time_pic)
 
     return totals
 
